a0876956e23ba7d38e5f77cdd7dd840ba0f7ac80
Audrico <drcsm2013@gmail.com>
2026-03-06 17:06:51 +0700

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 import { HeroSection } from "../components/sections/HeroSection";
 import { FeatureHighlight } from "../components/sections/FeatureHighlight";
 import { TemplateGallery } from "../components/sections/TemplateGallery";
-import { Pricing } from "../components/sections/Pricing";
+// import { Pricing } from "../components/sections/Pricing";
 import { useSettings } from "../contexts/SettingsContext";
 
 export const LandingPage = () => {
